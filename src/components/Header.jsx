f5bc38c48d2a9384f9c1f2f632a41e8ebc04c73f
Your NameAbbosbek-2010 <a21684787@gmail.com>
2026-01-30 14:34:52 +0500

    ozgardi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,13 @@ import './Header.css'
 const Header = () => {
   return (
     <div>
-
+      <div>
+        <nav>
+          <ul>
+            <li><h2>Amabar <br /> WOOD</h2></li>
+          </ul>
+        </nav>
+      </div>
     </div>
   )
 }
