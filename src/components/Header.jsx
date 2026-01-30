78e6a88cf4ff136e0fa03c2ba92427f39bfe6cf8
AniMaxa <optionsodilov@gmail.com>
2026-01-30 14:57:13 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <nav>
+          <ul>
+            <li><h2>Amabar <br /> WOOD</h2></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Section2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Section.css"
+
+const Section2 = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Section2
